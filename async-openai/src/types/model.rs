41f23eafad14c384867a934e8fe3601d8f9ae7df
kkbase <kkbase@163.com>
2025-04-10 16:30:11 +0800

    fix ListModelResponse decode json err

--- async-openai/src/types/model.rs
+++ async-openai/src/types/model.rs
@@ -4,24 +4,33 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct Model {
     /// The model identifier, which can be referenced in the API endpoints.
+    #[serde(default)]
     pub id: String,
     /// The object type, which is always "model".
+    #[serde(default)]
     pub object: String,
     /// The Unix timestamp (in seconds) when the model was created.
+    #[serde(default)]
     pub created: u32,
     /// The organization that owns the model.
+    #[serde(default)]
     pub owned_by: String,
 }
 
 #[derive(Debug, Deserialize, Clone, PartialEq, Serialize)]
 pub struct ListModelResponse {
+    #[serde(default)]
     pub object: String,
+    #[serde(default)]
     pub data: Vec<Model>,
 }
 
 #[derive(Debug, Deserialize, Clone, PartialEq, Serialize)]
 pub struct DeleteModelResponse {
+    #[serde(default)]
     pub id: String,
+    #[serde(default)]
     pub object: String,
+    #[serde(default)]
     pub deleted: bool,
 }
